954c36b8da765fcdad6f30d23945ce5b7f8f5ead
Pavel <93835948+pkorytkin@users.noreply.github.com>
2022-12-18 02:40:49 +0300

    Public Release

---
+++ NetworkConnectService/NetworkConnectService/NetworkConnectService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+namespace NetworkConnectService
+{
+    public partial class NetworkConnectService : ServiceBase
+    {
+        public NetworkConnectService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 10000; // 60 seconds
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.
+            //eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+
+            bool networkUp = CheckForInternetConnection();
+            Debug.WriteLine("" + networkUp);
+            if (!networkUp)
+            {
+                Disable("WiFi");
+                Thread.Sleep(3);
+                Enable("WiFi");
+            }
+        }
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "192.168.0.1";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+		//https://stackoverflow.com/questions/14424338/netsh-interface-set-interface-name-local-area-connection-2-admin-disabled
+        static void Enable(string interfaceName)
+        {
+            System.Diagnostics.ProcessStartInfo psi =
+                   new System.Diagnostics.ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        static void Disable(string interfaceName)
+        {
+            System.Diagnostics.ProcessStartInfo psi =
+                new System.Diagnostics.ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+    }
+}
---
+++ NetworkConnectService/NetworkConnectService/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkConnectService
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new NetworkConnectService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
